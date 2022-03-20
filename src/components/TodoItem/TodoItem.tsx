3d7b698d526fd1d6971f9ffe8b65303880499bd0
Oleksii Yastrebtsev <aldevid9@gmail.com>
2022-03-20 19:05:16 +0200

    add remove todo item feature

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -1,5 +1,6 @@
 import './TodoItem.scss';
 
+import { useRemoveTodoItem } from '@hooks/useRemoveTodoItem/useRemoveTodoItem';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { IconButton } from '@mui/material';
 import { Todo } from '@utils/types/todo';
@@ -7,11 +8,16 @@ import { Todo } from '@utils/types/todo';
 export type TodoProps = Readonly<Todo>;
 
 export const TodoItem = ({ id, text }: TodoProps) => {
+    const { removeTodoItem } = useRemoveTodoItem();
+
+    const onRemoveClick = () => {
+        removeTodoItem(id);
+    };
     return (
         <div data-testid={`todo-item-#${id}`}>
             <div>TODO with id - {id}</div>
             <div>{text}</div>
-            <IconButton color="error" aria-label="delete">
+            <IconButton onClick={onRemoveClick} color="error" aria-label="delete">
                 <DeleteIcon />
             </IconButton>
         </div>
