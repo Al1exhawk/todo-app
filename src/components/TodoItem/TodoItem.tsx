9ef2958d152659a2a2302ebbfac87340ae3350db
Oleksii Yastrebtsev <aldevid9@gmail.com>
2022-03-20 20:40:47 +0200

    refactoring

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -1,32 +1,39 @@
 import './TodoItem.scss';
 
-import { useRemoveTodoItem } from '@hooks/useRemoveTodoItem/useRemoveTodoItem';
 import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
 import { Checkbox, Grid, IconButton, Paper, Typography } from '@mui/material';
 import { Todo } from '@utils/types/todo';
 
-export type TodoProps = Readonly<Todo>;
+interface OwnProps {
+    editTodoItem: (newTodoItemValue: Todo) => void;
+    removeTodoItem: (id: number | string) => void;
+}
 
-export const TodoItem = ({ id, text, isDone }: TodoProps) => {
-    const { removeTodoItem } = useRemoveTodoItem();
+export type TodoProps = Readonly<Todo> & Readonly<OwnProps>;
+
+export const TodoItem = ({ id, text, isDone, removeTodoItem, editTodoItem }: TodoProps) => {
+    const onDoneChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+        editTodoItem({ id, text, isDone: target.checked });
+    };
 
     const onRemoveClick = () => {
         removeTodoItem(id);
     };
+
     return (
         <Paper>
-            <Grid
-                display="flex"
-                direction="row"
-                justifyContent="space-between"
-                alignItems="center"
-                data-testid={`todo-item-#${id}`}
-            >
-                <Checkbox checked={isDone} />
+            <Grid display="flex" justifyContent="space-between" alignItems="center" data-testid={`todo-item-#${id}`}>
+                <Checkbox checked={isDone} onChange={onDoneChange} color="success" />
                 <Typography>{text}</Typography>
-                <IconButton onClick={onRemoveClick} color="error" aria-label="delete">
-                    <DeleteIcon />
-                </IconButton>
+                <Grid>
+                    <IconButton color="warning" aria-label="delete">
+                        <EditIcon />
+                    </IconButton>
+                    <IconButton onClick={onRemoveClick} color="error" aria-label="delete">
+                        <DeleteIcon />
+                    </IconButton>
+                </Grid>
             </Grid>
         </Paper>
     );
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,17 +1,23 @@
 import './TodoList.scss';
 
 import { TodoItem } from '@components/TodoItem/TodoItem';
+import { useEditTodoItem } from '@hooks/useEditTodoItem/useEditTodoItem';
+import { useRemoveTodoItem } from '@hooks/useRemoveTodoItem/useRemoveTodoItem';
 import { Grid } from '@mui/material';
 import { searchTodosSelector } from '@store/selectors/todosSearch/todosSearch';
 import { useRecoilValue } from 'recoil';
 
 export function TodoList() {
     const todos = useRecoilValue(searchTodosSelector);
+
+    const { removeTodoItem } = useRemoveTodoItem();
+    const { editTodoItem } = useEditTodoItem();
+
     return (
         <Grid container item alignItems="center" display="grid" gap={5} direction="column" wrap="nowrap">
             {todos.map(({ id, ...rest }) => (
                 <Grid item xs={6} key={id}>
-                    <TodoItem id={id} {...rest} />
+                    <TodoItem id={id} removeTodoItem={removeTodoItem} editTodoItem={editTodoItem} {...rest} />
                 </Grid>
             ))}
         </Grid>
