a6c5ebf0ab014b67f9e669fbb0a9f86c1bd8f349
Oleksii Yastrebtsev <aldevid9@gmail.com>
2022-03-20 19:40:02 +0200

    add minor styles

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -2,24 +2,32 @@ import './TodoItem.scss';
 
 import { useRemoveTodoItem } from '@hooks/useRemoveTodoItem/useRemoveTodoItem';
 import DeleteIcon from '@mui/icons-material/Delete';
-import { IconButton } from '@mui/material';
+import { Checkbox, Grid, IconButton, Paper, Typography } from '@mui/material';
 import { Todo } from '@utils/types/todo';
 
 export type TodoProps = Readonly<Todo>;
 
-export const TodoItem = ({ id, text }: TodoProps) => {
+export const TodoItem = ({ id, text, isDone }: TodoProps) => {
     const { removeTodoItem } = useRemoveTodoItem();
 
     const onRemoveClick = () => {
         removeTodoItem(id);
     };
     return (
-        <div data-testid={`todo-item-#${id}`}>
-            <div>TODO with id - {id}</div>
-            <div>{text}</div>
-            <IconButton onClick={onRemoveClick} color="error" aria-label="delete">
-                <DeleteIcon />
-            </IconButton>
-        </div>
+        <Paper>
+            <Grid
+                display="flex"
+                direction="row"
+                justifyContent="space-between"
+                alignItems="center"
+                data-testid={`todo-item-#${id}`}
+            >
+                <Checkbox checked={isDone} />
+                <Typography>{text}</Typography>
+                <IconButton onClick={onRemoveClick} color="error" aria-label="delete">
+                    <DeleteIcon />
+                </IconButton>
+            </Grid>
+        </Paper>
     );
 };
--- src/views/TodoDashboardPage.tsx
+++ src/views/TodoDashboardPage.tsx
@@ -5,7 +5,7 @@ import { Grid } from '@mui/material';
 
 export function TodoDashboardPage() {
     return (
-        <Grid container display="grid" justifyContent="center" direction="column">
+        <Grid container display="grid" justifyContent="center" gap={2}>
             <Search />
             <TodoItemCreator />
             <TodoList />
